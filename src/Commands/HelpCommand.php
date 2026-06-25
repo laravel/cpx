ff07093790647ed7688c2e301f8a0c3afe27ce54
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-06-25 11:24:11 +0100

    Convert help command to Symfony

--- src/Commands/HelpCommand.php
+++ src/Commands/HelpCommand.php
@@ -4,29 +4,45 @@ declare(strict_types=1);
 
 namespace Cpx\Commands;
 
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'help',
+    description: 'Show the cpx help message',
+)]
 class HelpCommand extends Command
 {
-    public function __invoke(bool $unknownCommand = false): void
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        if ($unknownCommand) {
-            $this->error("Unrecognised command {$this->console->command}");
+        self::render($output);
+
+        return self::SUCCESS;
+    }
+
+    public static function render(OutputInterface $output, ?string $unknownCommand = null): void
+    {
+        if ($unknownCommand !== null) {
+            $output->writeln("<error>Unrecognised command {$unknownCommand}</error>");
         }
 
-        $this->success('cpx - A Composer package runner with on-demand execution and package management.');
-        $this->line('Usage:');
-        $this->line('  '.Command::COLOR_GREEN.'cpx <vendor/package[:version]> [args]   '.Command::COLOR_RESET.'Run a Composer package\'s bin command');
-        $this->line('  '.Command::COLOR_GREEN.'cpx check                               '.Command::COLOR_RESET.'Run a static analysis tool over a project');
-        $this->line('  '.Command::COLOR_GREEN.'cpx test                                '.Command::COLOR_RESET.'Run a testing framework over a project');
-        $this->line('  '.Command::COLOR_GREEN.'cpx format                              '.Command::COLOR_RESET.'Run a code formatter over a project');
-        $this->line('  '.Command::COLOR_GREEN.'cpx update                              '.Command::COLOR_RESET.'Update all packages');
-        $this->line('  '.Command::COLOR_GREEN.'cpx update <vendor/package>             '.Command::COLOR_RESET.'Update all versions of a package');
-        $this->line('  '.Command::COLOR_GREEN.'cpx clean                               '.Command::COLOR_RESET.'Clean unused packages (older than 30 days)');
-        $this->line('  '.Command::COLOR_GREEN.'cpx clean --all                         '.Command::COLOR_RESET.'Clean all packages');
-        $this->line('  '.Command::COLOR_GREEN.'cpx exec </path/to/php/file.php>        '.Command::COLOR_RESET.'Invoke a PHP file');
-        $this->line('  '.Command::COLOR_GREEN.'cpx exec -r <code>                      '.Command::COLOR_RESET.'Run PHP code without <?php ?> tags');
-        $this->line('  '.Command::COLOR_GREEN.'cpx tinker                              '.Command::COLOR_RESET.'Open an interactive REPL');
-        $this->line('  '.Command::COLOR_GREEN.'cpx list                                '.Command::COLOR_RESET.'List all installed packages');
-        $this->line('  '.Command::COLOR_GREEN.'cpx aliases                             '.Command::COLOR_RESET.'Show aliased package names to run via `cpx <alias>`');
-        $this->line('  '.Command::COLOR_GREEN.'cpx help                                '.Command::COLOR_RESET.'Show this help message');
+        $output->writeln('<info>cpx - A Composer package runner with on-demand execution and package management.</info>');
+        $output->writeln('Usage:');
+        $output->writeln('  <info>cpx <vendor/package[:version]> [args]   </info>Run a Composer package\'s bin command');
+        $output->writeln('  <info>cpx check                               </info>Run a static analysis tool over a project');
+        $output->writeln('  <info>cpx test                                </info>Run a testing framework over a project');
+        $output->writeln('  <info>cpx format                              </info>Run a code formatter over a project');
+        $output->writeln('  <info>cpx update                              </info>Update all packages');
+        $output->writeln('  <info>cpx update <vendor/package>             </info>Update all versions of a package');
+        $output->writeln('  <info>cpx clean                               </info>Clean unused packages (older than 30 days)');
+        $output->writeln('  <info>cpx clean --all                         </info>Clean all packages');
+        $output->writeln('  <info>cpx exec </path/to/php/file.php>        </info>Invoke a PHP file');
+        $output->writeln('  <info>cpx exec -r <code>                      </info>Run PHP code without <?php ?> tags');
+        $output->writeln('  <info>cpx tinker                              </info>Open an interactive REPL');
+        $output->writeln('  <info>cpx list                                </info>List all installed packages');
+        $output->writeln('  <info>cpx aliases                             </info>Show aliased package names to run via `cpx <alias>`');
+        $output->writeln('  <info>cpx help                                </info>Show this help message');
     }
 }
