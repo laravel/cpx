c2dbf18854176526ca48336c58aa2882407f51a3
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-06-25 11:24:05 +0100

    Convert format command to Symfony

--- src/Commands/FormatCommand.php
+++ src/Commands/FormatCommand.php
@@ -6,29 +6,47 @@ namespace Cpx\Commands;
 
 use Cpx\Console;
 use Cpx\Exceptions\ConsoleException;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'format',
+    description: 'Run a code formatter over a project',
+    aliases: ['fmt'],
+)]
 class FormatCommand extends Command
 {
-    public function __invoke(): void
+    protected function configure(): void
     {
-        $directory = $this->console->arguments[0] ?? '.';
+        $this->addArgument('directory', InputArgument::OPTIONAL, 'Directory to format', '.');
+        $this->addOption('dry-run', null, InputOption::VALUE_NONE, 'Report formatting changes without writing them');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $directory = $input->getArgument('directory');
+        $directory = is_string($directory) ? $directory : '.';
 
         if (file_exists('vendor/bin/pint')) {
             $command = "vendor/bin/pint {$directory}";
 
-            if ($this->console->hasOption('dry-run')) {
+            if ($input->getOption('dry-run') === true) {
                 $command .= ' --test';
             }
 
             Console::parse($command)->exec();
 
-            return;
+            return self::SUCCESS;
         }
 
         if (file_exists('vendor/bin/php-cs-fixer')) {
             $command = "vendor/bin/php-cs-fixer fix {$directory}";
 
-            if ($this->console->hasOption('dry-run')) {
+            if ($input->getOption('dry-run') === true) {
                 $command .= ' --dry-run';
             }
 
@@ -36,7 +54,7 @@ class FormatCommand extends Command
 
             Console::parse($command)->exec();
 
-            return;
+            return self::SUCCESS;
         }
 
         if (file_exists('vendor/bin/phpcbf')) {
@@ -44,7 +62,7 @@ class FormatCommand extends Command
 
             Console::parse($command)->exec();
 
-            return;
+            return self::SUCCESS;
         }
 
         throw new ConsoleException('No code formatters found in the project.');
