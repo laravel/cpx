0a4045ebfd96fcc92e9991c66fb17c1ec1195aab
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-06-25 11:23:57 +0100

    Convert exec command to Symfony

--- src/Commands/ExecCommand.php
+++ src/Commands/ExecCommand.php
@@ -5,20 +5,54 @@ declare(strict_types=1);
 namespace Cpx\Commands;
 
 use Cpx\PhpExecutionHelper;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'exec',
+    description: 'Invoke a PHP file or inline PHP code',
+)]
 class ExecCommand extends Command
 {
     public string $path;
 
-    public function __invoke(): void
+    protected function configure(): void
     {
-        if ($this->console->hasOption('r')) {
-            $code = $this->console->getOption('r');
+        $this->addArgument('file', InputArgument::OPTIONAL, 'PHP file to invoke');
+        $this->addOption('run', 'r', InputOption::VALUE_REQUIRED, 'Run PHP code without <?php ?> tags');
+        $this->addOption('find-autoloader', null, InputOption::VALUE_OPTIONAL, 'Find and load the nearest Composer autoloader', 'true');
+        $this->addOption('load-laravel-bootstrap', null, InputOption::VALUE_OPTIONAL, 'Load Laravel bootstrap files when available', 'true');
+        $this->addOption('alias-classes', null, InputOption::VALUE_OPTIONAL, 'Alias classes from loaded autoloaders', 'true');
+    }
 
-            if (empty($code)) {
-                $this->error('Please supply code to execute with the -r option.');
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        ob_start();
 
-                return;
+        try {
+            return $this->executeInput($input, $output);
+        } finally {
+            $contents = ob_get_clean();
+
+            if ($contents !== false) {
+                $output->write($contents);
+            }
+        }
+    }
+
+    private function executeInput(InputInterface $input, OutputInterface $output): int
+    {
+        if ($input->getOption('run') !== null) {
+            $code = $input->getOption('run');
+
+            if (! is_string($code) || $code === '') {
+                $output->writeln('<error>Please supply code to execute with the -r option.</error>');
+
+                return self::FAILURE;
             }
 
             if (str_starts_with($code, '<?php')) {
@@ -36,53 +70,57 @@ class ExecCommand extends Command
             $directory = getcwd();
 
             if ($directory === false) {
-                $this->error('Unable to determine the current working directory.');
+                $output->writeln('<error>Unable to determine the current working directory.</error>');
 
-                return;
+                return self::FAILURE;
             }
 
-            $this->autoload($directory);
+            $this->autoload($directory, $input, $output);
 
             eval($code);
             echo PHP_EOL;
 
-            return;
+            return self::SUCCESS;
         }
 
-        if (empty($this->console->arguments[0])) {
-            $this->error('Please supply the path to a file to execute.');
+        $file = $input->getArgument('file');
 
-            return;
+        if (! is_string($file) || $file === '') {
+            $output->writeln('<error>Please supply the path to a file to execute.</error>');
+
+            return self::FAILURE;
         }
 
-        $path = realpath($this->console->arguments[0]);
+        $path = realpath($file);
 
         if ($path === false || ! file_exists($path)) {
-            $this->error("File does not exist at '{$this->console->arguments[0]}'");
+            $output->writeln("<error>File does not exist at '{$file}'</error>");
 
-            return;
+            return self::FAILURE;
         }
 
         $this->path = $path;
 
-        $this->autoload(dirname($this->path));
+        $this->autoload(dirname($this->path), $input, $output);
 
         $this->runFile();
+
+        return self::SUCCESS;
     }
 
-    protected function autoload(string $directory): void
+    protected function autoload(string $directory, InputInterface $input, OutputInterface $output): void
     {
-        $shouldFindAutoloader = $this->booleanOption('find-autoloader', true);
-        $shouldLoadLaravelBootstrap = $this->booleanOption('load-laravel-bootstrap', true);
-        $shouldAliasClasses = $this->booleanOption('alias-classes', true);
-        $shouldBeVerbose = $this->booleanOption('verbose', false);
+        $shouldFindAutoloader = $this->booleanOption($input, 'find-autoloader', true);
+        $shouldLoadLaravelBootstrap = $this->booleanOption($input, 'load-laravel-bootstrap', true);
+        $shouldAliasClasses = $this->booleanOption($input, 'alias-classes', true);
+        $shouldBeVerbose = $output->isVerbose();
 
         PhpExecutionHelper::init($directory, $shouldFindAutoloader, $shouldLoadLaravelBootstrap, $shouldAliasClasses, $shouldBeVerbose);
     }
 
-    protected function booleanOption(string $option, bool $default): bool
+    protected function booleanOption(InputInterface $input, string $option, bool $default): bool
     {
-        $value = $this->console->getOption($option);
+        $value = $input->getOption($option);
 
         if ($value === null) {
             return $default;
