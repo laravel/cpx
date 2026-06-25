13701ad4f34de40e31d19f2da5a7a5633ca5844d
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-06-25 11:23:35 +0100

    Convert aliases command to Symfony

--- src/Commands/AliasesCommand.php
+++ src/Commands/AliasesCommand.php
@@ -5,18 +5,28 @@ declare(strict_types=1);
 namespace Cpx\Commands;
 
 use Cpx\PackageAliases;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'aliases',
+    description: 'Show aliased package commands',
+)]
 class AliasesCommand extends Command
 {
-    public function __invoke(): void
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->line('Aliased packages:'.PHP_EOL);
+        $output->writeln('Aliased packages:'.PHP_EOL);
         $packages = PackageAliases::$packages;
         usort($packages, fn (array $a, array $b): int => strcmp($a['command'], $b['command']));
 
         foreach ($packages as $package) {
             $paddedCommand = str_pad($package['command'], 15);
-            $this->line('  '.Command::COLOR_GREEN.'cpx '.$paddedCommand.Command::COLOR_RESET.'   '.$package['description']);
+            $output->writeln('  <info>cpx '.$paddedCommand.'</info>   '.$package['description']);
         }
+
+        return self::SUCCESS;
     }
 }
