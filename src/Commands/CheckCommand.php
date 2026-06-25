fc867ddbfb01d5511727bb82978b58be2aa44b26
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-06-25 11:23:43 +0100

    Convert check command to Symfony

--- src/Commands/CheckCommand.php
+++ src/Commands/CheckCommand.php
@@ -6,17 +6,26 @@ namespace Cpx\Commands;
 
 use Cpx\Console;
 use Cpx\Exceptions\ConsoleException;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'check',
+    description: 'Run a static analysis tool over a project',
+    aliases: ['analyze', 'analyse'],
+)]
 class CheckCommand extends Command
 {
-    public function __invoke(): void
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         if (file_exists('vendor/bin/phpstan')) {
             $command = 'vendor/bin/phpstan analyse';
 
             Console::parse($command)->exec();
 
-            return;
+            return self::SUCCESS;
         }
 
         if (file_exists('vendor/bin/psalm')) {
@@ -24,7 +33,7 @@ class CheckCommand extends Command
 
             Console::parse($command)->exec();
 
-            return;
+            return self::SUCCESS;
         }
 
         if (file_exists('vendor/bin/phan')) {
@@ -32,7 +41,7 @@ class CheckCommand extends Command
 
             Console::parse($command)->exec();
 
-            return;
+            return self::SUCCESS;
         }
 
         throw new ConsoleException('No static analyzers found in the project.');
