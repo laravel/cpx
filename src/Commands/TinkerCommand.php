e9c58c1664844e0a4dc2dd848540fb6c0dda7cd6
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-06-25 11:24:42 +0100

    Convert tinker command to Symfony

--- src/Commands/TinkerCommand.php
+++ src/Commands/TinkerCommand.php
@@ -6,19 +6,39 @@ namespace Cpx\Commands;
 
 use Cpx\Console;
 use Cpx\Package;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'tinker',
+    description: 'Open an interactive REPL',
+)]
 class TinkerCommand extends Command
 {
-    public function __invoke(): void
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $psyshConfig = realpath(__DIR__.'/../../files/psysh-config.php');
 
         if ($psyshConfig === false) {
-            $this->error('Unable to find the PsySH configuration file.');
+            $output->writeln('<error>Unable to find the PsySH configuration file.</error>');
 
-            return;
+            return self::FAILURE;
         }
 
-        Package::parse('psy/psysh')->runCommand(Console::parse("psysh --config {$psyshConfig}"));
+        ob_start();
+
+        try {
+            Package::parse('psy/psysh')->runCommand(Console::parse("psysh --config {$psyshConfig}"));
+        } finally {
+            $contents = ob_get_clean();
+
+            if ($contents !== false) {
+                $output->write($contents);
+            }
+        }
+
+        return self::SUCCESS;
     }
 }
