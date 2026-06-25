955d2e350eb51e6c6b7ac4bb11c733b3744372b9
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-06-25 11:24:27 +0100

    Convert package fallback command to Symfony

--- src/Commands/RunPackageCommand.php
+++ src/Commands/RunPackageCommand.php
@@ -4,7 +4,47 @@ declare(strict_types=1);
 
 namespace Cpx\Commands;
 
-class RunPackageCommand extends Command
+use Cpx\Console;
+use Cpx\PackageCommandRunner;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: self::Name,
+    hidden: true,
+)]
+class RunPackageCommand extends SymfonyCommand
 {
-    public function __invoke(): void {}
+    public const Name = '__cpx_run_package';
+
+    public function __construct(
+        private PackageCommandRunner $packageCommandRunner,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->ignoreValidationErrors();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $console = Console::parse($input instanceof ArgvInput ? $input->getRawTokens() : []);
+
+        ob_start();
+
+        try {
+            return $this->packageCommandRunner->run($console, $output);
+        } finally {
+            $contents = ob_get_clean();
+
+            if ($contents !== false) {
+                $output->write($contents);
+            }
+        }
+    }
 }
