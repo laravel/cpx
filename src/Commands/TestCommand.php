81a247c934e5596f11918b3e3ca37d52916bd772
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-06-25 11:24:35 +0100

    Convert test command to Symfony

--- src/Commands/TestCommand.php
+++ src/Commands/TestCommand.php
@@ -6,33 +6,41 @@ namespace Cpx\Commands;
 
 use Cpx\Console;
 use Cpx\Exceptions\ConsoleException;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(
+    name: 'test',
+    description: 'Run a testing framework over a project',
+)]
 class TestCommand extends Command
 {
-    public function __invoke(): void
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         if (file_exists('vendor/bin/pest')) {
             Console::parse('vendor/bin/pest')->exec();
 
-            return;
+            return self::SUCCESS;
         }
 
         if (file_exists('bin/phpunit')) {
             Console::parse('bin/phpunit')->exec();
 
-            return;
+            return self::SUCCESS;
         }
 
         if (file_exists('vendor/bin/phpunit')) {
             Console::parse('vendor/bin/phpunit')->exec();
 
-            return;
+            return self::SUCCESS;
         }
 
         if (file_exists('vendor/bin/codecept')) {
             Console::parse('vendor/bin/codecept')->exec();
 
-            return;
+            return self::SUCCESS;
         }
 
         throw new ConsoleException('No test runner found in the project.');
