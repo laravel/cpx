2b02ad08e81e697f3cc62dc2a85bbcb9b0c9546b
Titas Gailius <titas.gailius@gmail.com>
2026-07-01 15:41:11 +0300

    Update AliasesCommand.php

--- src/Commands/AliasesCommand.php
+++ src/Commands/AliasesCommand.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace Cpx\Commands;
 
+use Cpx\Packages\Package;
 use Cpx\Packages\PackageAlias;
 use Cpx\Packages\PackageAliases;
 use Cpx\Packages\UserAliases;
+use Laravel\Prompts\Prompt;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+
+use function Laravel\Prompts\info;
+use function Laravel\Prompts\table;
 
 #[AsCommand(
     name: 'aliases',
@@ -20,6 +25,8 @@ class AliasesCommand extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        Prompt::setOutput($output);
+
         $output->writeln('Aliased packages:'.PHP_EOL);
         $packages = PackageAliases::all();
         usort($packages, fn (PackageAlias $a, PackageAlias $b): int => strcmp($a->command, $b->command));
@@ -34,12 +41,16 @@ class AliasesCommand extends Command
         if ($userAliases !== []) {
             ksort($userAliases);
 
-            $output->writeln(PHP_EOL.'Your aliases:'.PHP_EOL);
+            info('Your aliases:');
 
-            foreach ($userAliases as $name => $package) {
-                $paddedCommand = str_pad($name, 15);
-                $output->writeln('  <info>cpx '.$paddedCommand.'</info>   '.$package->fullPackageString());
-            }
+            table(
+                headers: ['Alias', 'Package'],
+                rows: array_map(
+                    fn (string $name, Package $package): array => ['cpx '.$name, $package->fullPackageString()],
+                    array_keys($userAliases),
+                    $userAliases,
+                ),
+            );
         }
 
         return self::SUCCESS;
