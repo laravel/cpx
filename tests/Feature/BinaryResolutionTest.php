6a807f5d5f3048c3630e866711628bab99053ec6
Lazizbek Ergashev <lazerg2@gmail.com>
2026-07-01 21:51:15 +0500

    Add coverage for positional arguments with multiple binaries (#20)
    * Add coverage for positional arguments with multiple binaries
    
    * Update ExecCommand.php
    
    ---------
    
    Co-authored-by: Joe Tannenbaum <joe.tannenbaum@laravel.com>

--- tests/Feature/BinaryResolutionTest.php
+++ tests/Feature/BinaryResolutionTest.php
@@ -29,6 +29,21 @@ test('a package with multiple binaries uses the first forwarded argument as the 
         ->and(json_decode((string) file_get_contents($logFile), true))->toBe(['--flag']);
 });
 
+test('a package with multiple binaries keeps a positional argument when a binary matches the package name', function () {
+    $this->useIsolatedComposerHome();
+    $logFile = $this->temporaryDirectory('cpx-log').'/argv.json';
+
+    prepareCachedPackage('vendor/package', ['package', 'other'], [
+        'package' => "#!/usr/bin/env php\n<?php file_put_contents('{$logFile}', json_encode(array_slice(\$argv, 1), JSON_THROW_ON_ERROR)); exit(0);\n",
+        'other' => "#!/usr/bin/env php\n<?php exit(99);\n",
+    ]);
+
+    [$status] = runCpxCommand(['vendor/package', 'tests/Sub']);
+
+    expect($status)->toBe(0)
+        ->and(json_decode((string) file_get_contents($logFile), true))->toBe(['tests/Sub']);
+});
+
 test('ambiguous multiple-binary packages list the available binaries', function () {
     $this->useIsolatedComposerHome();
 
