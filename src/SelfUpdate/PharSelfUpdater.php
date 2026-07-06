59ddcf4c3163cd5036a5874c2ffe3a199dabea27
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-07-06 13:25:09 +0100

    Let pre-release builds self-update to any stability

--- src/SelfUpdate/PharSelfUpdater.php
+++ src/SelfUpdate/PharSelfUpdater.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Cpx\SelfUpdate;
 
+use Composer\Semver\VersionParser;
 use Cpx\Version;
 use Humbug\SelfUpdate\Strategy\GithubStrategy;
 use Humbug\SelfUpdate\Updater;
@@ -14,13 +15,23 @@ class PharSelfUpdater implements SelfUpdater
 
     public function __construct()
     {
+        $version = Version::resolve();
+
         $strategy = new GithubStrategy;
         $strategy->setPackageName('cpx/cpx');
         $strategy->setPharName('cpx');
-        $strategy->setCurrentLocalVersion(Version::resolve());
+        $strategy->setCurrentLocalVersion($version);
+        $strategy->setStability(self::stabilityFor($version));
 
         $this->updater = new Updater(null, false);
         $this->updater->setStrategyObject($strategy);
+    }
+
+    public static function stabilityFor(string $version): string
+    {
+        return VersionParser::parseStability($version) === 'stable'
+            ? GithubStrategy::STABLE
+            : GithubStrategy::ANY;
     }
 
     public function update(): bool
