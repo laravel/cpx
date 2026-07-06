f5cd277a16cce5c291aad146781ea635eef23ea8
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-07-06 13:25:01 +0100

    Load the tinker config through the phar in a child process

--- src/Commands/TinkerCommand.php
+++ src/Commands/TinkerCommand.php
@@ -6,6 +6,8 @@ namespace Cpx\Commands;
 
 use Cpx\Input\PackageInvocation;
 use Cpx\Packages\Package;
+use Cpx\Runtime\Environment;
+use Cpx\Support\Filesystem;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -19,9 +21,9 @@ class TinkerCommand extends Command
 {
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $psyshConfig = realpath(__DIR__.'/../../files/psysh-config.php');
+        $psyshConfig = $this->psyshConfigPath();
 
-        if ($psyshConfig === false) {
+        if ($psyshConfig === null) {
             $output->writeln('<error>Unable to find the PsySH configuration file.</error>');
 
             return self::FAILURE;
@@ -31,5 +33,28 @@ class TinkerCommand extends Command
             PackageInvocation::fromRawTokens(['psysh', '--config', $psyshConfig]),
             $output,
         );
+    }
+
+    private function psyshConfigPath(): ?string
+    {
+        $pharPath = Environment::pharPath();
+
+        if ($pharPath === '') {
+            $bundled = __DIR__.'/../../files/psysh-config.php';
+
+            return file_exists($bundled) ? $bundled : null;
+        }
+
+        // The psysh child process cannot read phar:// paths, so hand it a stub that loads the phar first.
+        $config = cpx_path('psysh-config.php');
+        $stub = sprintf(
+            "<?php\n\nPhar::loadPhar('%s', 'cpx.phar');\n\nreturn require 'phar://cpx.phar/files/psysh-config.php';\n",
+            addcslashes($pharPath, "\\'"),
+        );
+
+        Filesystem::ensureDirectory(cpx_path());
+        Filesystem::writeAtomic($config, $stub);
+
+        return $config;
     }
 }
