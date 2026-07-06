d62c2ba0c750e43e5e742b725c7dc5d57c9d1af8
Titas Gailius <titas.gailius@gmail.com>
2026-07-06 11:23:30 +0300

    MalformedAliasesException

---
+++ src/Exceptions/MalformedAliasesException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cpx\Exceptions;
+
+use Exception;
+
+class MalformedAliasesException extends Exception
+{
+    public function __construct(string $file)
+    {
+        parent::__construct("The aliases file at {$file} is malformed. Expected a JSON object mapping alias names to packages.");
+    }
+}
--- src/Packages/UserAliases.php
+++ src/Packages/UserAliases.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Cpx\Packages;
 
+use Cpx\Exceptions\MalformedAliasesException;
 use Cpx\Support\Filesystem;
 
 class UserAliases
@@ -17,6 +18,9 @@ class UserAliases
         protected array $aliases = [],
     ) {}
 
+    /**
+     * @throws MalformedAliasesException
+     */
     public static function open(): self
     {
         $file = cpx_path(self::FILE);
@@ -26,6 +30,16 @@ class UserAliases
         }
 
         $json = json_decode((string) file_get_contents($file), true);
+
+        if (! is_array($json)) {
+            throw new MalformedAliasesException($file);
+        }
+
+        foreach ($json as $name => $value) {
+            if (! is_string($name) || ! is_string($value)) {
+                throw new MalformedAliasesException($file);
+            }
+        }
 
         return new self(array_map(
             fn (string $value): Package => Package::parse($value),
