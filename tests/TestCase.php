b9cd87cab4ac56c8936a18c98f7ccf1ca685aee8
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-07-09 19:16:38 +0100

    Keep test child processes off the paratest command pipe

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -34,6 +34,9 @@ abstract class TestCase extends BaseTestCase
         Prompt::interactive(false);
         Prompt::setOutput(new BufferedConsoleOutput);
 
+        // Keep child processes off the real STDIN: under pest --parallel it is the paratest worker's command pipe.
+        ProcessRunner::fakeInput('');
+
         (new ReflectionProperty(Prompt::class, 'terminal'))->setValue(null, new Terminal);
         (new ReflectionProperty(Prompt::class, 'shouldFallback'))->setValue(null, false);
         (new ReflectionProperty(Prompt::class, 'fallbacks'))->setValue(null, []);
