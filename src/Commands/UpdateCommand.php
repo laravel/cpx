edd863888b86a4ee12a0946e4f2c95be399bd028
Wendell Adriel <wendelladriel.ti@gmail.com>
2026-07-17 15:17:35 +0100

    Continue update past failing packages with per-package reasons

--- src/Commands/UpdateCommand.php
+++ src/Commands/UpdateCommand.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Cpx\Commands;
 
+use Cpx\Commands\Concerns\OutputsJson;
 use Cpx\Composer\ComposerRunner;
+use Cpx\Exceptions\ComposerCommandException;
 use Cpx\Packages\Package;
 use Cpx\Process\ProcessRunner;
 use Cpx\Support\Filesystem;
+use Laravel\Prompts\Elements\Element;
 use Laravel\Prompts\Support\Logger;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -15,6 +18,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use function Laravel\Prompts\callout;
 use function Laravel\Prompts\info;
 use function Laravel\Prompts\task;
 
@@ -24,13 +28,28 @@ use function Laravel\Prompts\task;
 )]
 class UpdateCommand extends Command
 {
+    use OutputsJson;
+
+    private bool $json = false;
+
+    /** @var list<array{package: string, updated: bool, from: string, to: string, reason: string|null}> */
+    private array $packages = [];
+
+    /** @var list<string> */
+    private array $errors = [];
+
     protected function configure(): void
     {
         $this->addArgument('target', InputArgument::OPTIONAL, 'Package or vendor to update');
+        $this->addJsonOption();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $this->json = $this->wantsJson($input);
+        $this->packages = [];
+        $this->errors = [];
+
         $target = (string) $input->getArgument('target');
 
         match (true) {
@@ -39,7 +58,26 @@ class UpdateCommand extends Command
             default => $this->updateAllPackages(),
         };
 
-        return self::SUCCESS;
+        if ($this->json) {
+            return $this->errors === []
+                ? $this->outputJsonSuccess($output, ['packages' => $this->packages])
+                : $this->outputJsonFailure($output, $this->errors, ['packages' => $this->packages]);
+        }
+
+        if ($this->errors === []) {
+            return self::SUCCESS;
+        }
+
+        callout(
+            label: 'Update Summary',
+            content: [
+                Element::heading('Could not update'),
+                Element::bulletedList($this->errors),
+            ],
+            type: 'warning',
+        );
+
+        return self::FAILURE;
     }
 
     protected function updateAllPackages(): void
@@ -47,7 +85,9 @@ class UpdateCommand extends Command
         $packageDirectories = glob(cpx_path('*/*/*'), GLOB_ONLYDIR) ?: [];
 
         if (empty($packageDirectories)) {
-            info('There are no packages to update.');
+            if (! $this->json) {
+                info('There are no packages to update.');
+            }
         } else {
             foreach ($packageDirectories as $directory) {
                 $this->updateDirectory($directory);
@@ -60,7 +100,9 @@ class UpdateCommand extends Command
         $packageDirectories = glob(cpx_path("{$vendor}/*/*"), GLOB_ONLYDIR) ?: [];
 
         if (empty($packageDirectories)) {
-            info("There are no packages in vendor '{$vendor}' to update.");
+            if (! $this->json) {
+                info("There are no packages in vendor '{$vendor}' to update.");
+            }
         } else {
             foreach ($packageDirectories as $directory) {
                 $this->updateDirectory($directory);
@@ -79,7 +121,9 @@ class UpdateCommand extends Command
         $packageDirectories = glob(cpx_path("{$package->vendor}/{$package->name}/*"), GLOB_ONLYDIR) ?: [];
 
         if (empty($packageDirectories)) {
-            info("There are no installed versions of '{$package->vendor}/{$package->name}' to update.");
+            if (! $this->json) {
+                info("There are no installed versions of '{$package->vendor}/{$package->name}' to update.");
+            }
         } else {
             foreach ($packageDirectories as $directory) {
                 $this->updateDirectory($directory);
@@ -92,11 +136,46 @@ class UpdateCommand extends Command
         $relative = ltrim(str_replace(Filesystem::normalizePath(cpx_path()), '', Filesystem::normalizePath($directory)), '/');
         $package = implode('/', array_slice(explode('/', $relative), 0, 2));
 
+        if ($this->json) {
+            $previousVersion = ComposerRunner::getCurrentVersion($directory, $package);
+            $error = null;
+
+            try {
+                ProcessRunner::withLogger(new Logger('cpx'), fn () => ComposerRunner::run(['update'], $directory));
+            } catch (ComposerCommandException $exception) {
+                $error = $exception->getMessage();
+                $this->errors[] = "{$relative}: {$error}";
+            }
+
+            $newVersion = ComposerRunner::getCurrentVersion($directory, $package);
+            $updated = $error === null && $previousVersion !== $newVersion;
+
+            $this->packages[] = [
+                'package' => $relative,
+                'updated' => $updated,
+                'from' => $previousVersion,
+                'to' => $newVersion,
+                'reason' => $error ?? ($updated ? null : 'already up-to-date'),
+            ];
+
+            return;
+        }
+
         task(
             label: "Updating {$relative}",
             callback: function (Logger $logger) use ($directory, $relative, $package): void {
                 $previousVersion = ComposerRunner::getCurrentVersion($directory, $package);
-                ProcessRunner::withLogger($logger, fn () => ComposerRunner::run(['update'], $directory));
+
+                try {
+                    ProcessRunner::withLogger($logger, fn () => ComposerRunner::run(['update'], $directory));
+                } catch (ComposerCommandException $exception) {
+                    $this->errors[] = "{$relative}: {$exception->getMessage()}";
+                    $logger->error($exception->getMessage());
+                    $logger->label("{$relative} could not be updated");
+
+                    return;
+                }
+
                 $newVersion = ComposerRunner::getCurrentVersion($directory, $package);
 
                 if ($previousVersion !== $newVersion) {
